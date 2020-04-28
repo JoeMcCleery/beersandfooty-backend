8444c5d6c60ea9abea33d12107b5431b258b1f85
joe <josephm@strangeanimals.com.au>
2020-04-28 16:37:06 +0800

    master: -update welcome.blade.php -add package-lock.json -update User model to have first_name and last_name -add UserController -add makefile with some common artisan commands

---
+++ Makefile
@@ -0,0 +1,8 @@
+serve:
+	php artisan serve
+
+migrate:
+	php artisan migrate
+
+rollback:
+	php artisan migrate:reset
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Beers and Footy - API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -58,6 +58,10 @@
                 text-transform: uppercase;
             }
 
+            .links > a:hover {
+                text-decoration: underline;
+            }
+
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -81,18 +85,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Beers and Footy API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://beersandfooty.com">Goto Live Website</a>
                 </div>
             </div>
         </div>
