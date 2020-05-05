bfd158495b2c8be5423d29ca3503a0e463684517
joe <josephm@strangeanimals.com.au>
2020-05-05 16:10:37 +0800

    master: -route comments

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// GET Reviews and Review Content
+// GET all Reviews
 Route::get('/reviews', function (Request $request) {
     if($request->type) {
         return new ReviewCollection(Review::where(['type' => $request->type])->paginate());
     }
     return new ReviewCollection(Review::paginate());
 });
+// GET Review by id
 Route::get('/review/{id}', function (Request $request) {
     return new ReviewResource(Review::find($request->id));
 });
