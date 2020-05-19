23fc9e66fecc8ad71704e20bd9057b3c872e57e5
joe <josephm@strangeanimals.com.au>
2020-05-19 17:48:09 +0800

    master: -mdunno what doin

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Beers and Footy API - Site in Maintenance'))
 @section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Site in Maintenance'))
+@section('message', __('Site in Maintenance'))
