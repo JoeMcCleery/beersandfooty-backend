d8d2210ec83466745aee0c46081276c3a2dfc52a
joe <josephm@strangeanimals.com.au>
2020-06-01 17:42:57 +0800

    master: -paginate all lists

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return new UserCollection(User::all());
+        return new UserCollection(User::all()->paginate());
     }
 
     public function show($id)
