0198347a9b1c1f80b9e10f618cce7c2e9fa5c536
joe <josephm@strangeanimals.com.au>
2020-06-01 11:06:18 +0800

    master: -only use password and credentials grants

--- app/Http/Controllers/api/v1/LoginController.php
+++ app/Http/Controllers/api/v1/LoginController.php
@@ -6,7 +6,6 @@ use App\EloquentModels\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Passport\Client;
 
 class LoginController extends Controller
 {
@@ -17,36 +16,30 @@ class LoginController extends Controller
             'username' => 'required|string',
             'password' => 'required|string'
         ]);
-
         if (!$register) {
             return response([
                 'success' => false,
                 'message' => 'Could not validate Credentials'
             ]);
         }
-
         $username = $request->username;
         $password = $request->password;
-
         if (!User::where(['username' => $username])->get()->isEmpty()) {
             return response([
                 'success' => false,
                 'message' => 'Username is already in use'
             ]);
         }
-
         $user = User::create([
             'username' => $username,
             'password' => $password
         ]);
-
         auth()->login($user);
         $user = Auth::user();
-
         return response([
             'success' => true,
             'data' => [
-                'user' => $user,
+                'user' => $user
             ]
         ]);
     }
@@ -57,27 +50,23 @@ class LoginController extends Controller
             'username' => 'required|string',
             'password' => 'required|string'
         ]);
-
         if (!$login) {
             return response([
                 'success' => false,
                 'message' => 'Could not validate Credentials'
             ]);
         }
-
         if (!Auth::attempt($login)) {
             return response([
                 'success' => false,
                 'message' => 'Invalid login credentials'
             ]);
         }
-
         $user = Auth::user();
-
         return response([
             'success' => true,
             'data' => [
-                'user' => $user,
+                'user' => $user
             ]
         ]);
     }
