3ba538b4d2188f8efac925add1e7d09e2b1dce59
joe <josephm@strangeanimals.com.au>
2020-06-27 14:10:51 +0800

    master: -order user reviews in resource by publish date

--- app/EloquentModels/User.php
+++ app/EloquentModels/User.php
@@ -100,7 +100,7 @@ class User extends Authenticatable
      */
     public function reviews()
     {
-        return $this->hasMany(Review::class);
+        return $this->hasMany(Review::class)->orderBy('publish_date', 'desc');
     }
 
     /**
