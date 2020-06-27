c5b6512b6297d2017c549eceb6284a1802b27be7
joe <josephm@strangeanimals.com.au>
2020-06-27 13:12:18 +0800

    master: -updooots

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,7 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'username' => $faker->name,
+        'email' => $faker->email,
         'password' => 'password', // password
     ];
 });
